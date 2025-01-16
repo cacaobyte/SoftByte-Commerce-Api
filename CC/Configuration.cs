5cf531013dda78bd960bce56b6865a73d7eb4607
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-01-15 23:37:56 -0600

    Configuración inicial de las variables de entorno

--- CC/Configuration.cs
+++ CC/Configuration.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace CC
 {
-    internal class Configuration
+    public class Configuration
     {
+        private IConfiguration appsettingsFile { get;  }
     }
 }
--- CC/Configurations/AppSettings/AppSettings.cs
+++ CC/Configurations/AppSettings/AppSettings.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace CC.Configurations.AppSettings
 {
-    internal class AppSettings
+    public class AppSettings
     {
+        private IConfiguration appsettingsFile { get; }
+
+        public AppSettings(IConfiguration configuration) {
+            appsettingsFile = configuration;
+        }
     }
 }
