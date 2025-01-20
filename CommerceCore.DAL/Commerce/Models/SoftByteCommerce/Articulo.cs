199a710709e1211feb825ee016f9dc3b10a9d1c7
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-01-20 01:51:44 -0600

    scalffodin de las nuevas tablas

--- CommerceCore.DAL/Commerce/Models/SoftByteCommerce/Articulo.cs
+++ CommerceCore.DAL/Commerce/Models/SoftByteCommerce/Articulo.cs
@@ -25,11 +25,14 @@ public partial class Articulo
 
     public string? Updatedby { get; set; }
 
-    public string? SubCategoria { get; set; }
-
     public DateTime? Fechacreacion { get; set; }
 
     public DateTime? Fechaactualizacion { get; set; }
+
+    /// <summary>
+    /// Se usa para guardar las subcategorías de los productos
+    /// </summary>
+    public string? SubCategoria { get; set; }
 
     public virtual ICollection<ExistenciaBodega> ExistenciaBodegas { get; set; } = new List<ExistenciaBodega>();
 }
