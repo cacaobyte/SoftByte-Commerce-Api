07f7e3df7b524e62cafde1876ae2dde20596a4f9
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-02-06 06:30:40 -0600

    Agregando rutas para traer faqs y guias

---
+++ CommerceCore.Api/Controllers/cc/supports/FaqsSupport/FaqsController.cs
@@ -0,0 +1,37 @@
+﻿using CommerceCore.Api.Tools;
+using CommerceCore.BL.cc.logistics;
+using Microsoft.AspNetCore.Mvc;
+using CommerceCore.BL.cc.supports.Faqs;
+
+namespace CommerceCore.Api.Controllers.cc.supports.FaqsSupport
+{
+    [Route("faqs")]
+    [ApiController]
+    public class FaqsController : CustomController
+    {
+        /// <summary>
+        /// Bl para manejar preguntas frecuentes
+        /// </summary>
+        /// <returns></returns>
+        private Faqs blFaqs { get; } = new Faqs(Tool.configuration);
+
+        /// <summary>
+        /// Devuelve todas las preguntas frecuentes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("allFaqs")]
+        public IActionResult GetAllFaqs()
+        {
+            try
+            {
+                  var result = blFaqs.GetFaqs();
+          
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while retrieving the articles.{ex}");
+            }
+        }
+    }
+}
