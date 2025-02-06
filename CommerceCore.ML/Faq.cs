1fc4f4cf79111e8a1a3694d268d0d64126d0cac5
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-02-05 22:43:39 -0600

    scalffolding de faqs, guia, cliente, vendedores

--- CommerceCore.ML/Faq.cs
+++ CommerceCore.ML/Faq.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace CommerceCore.ML
 {
-    internal class Faq
+    public partial class Faq
     {
+        public int Id { get; set; }
+
+        public string Pregunta { get; set; } = null!;
+
+        public string Respuesta { get; set; } = null!;
+
+        public string? Categoria { get; set; }
+
+        public string? Subcategoria { get; set; }
+
+        public string? Keywords { get; set; }
+
+        public int? Visitas { get; set; }
+
+        public int? Importancia { get; set; }
+
+        public int? Orden { get; set; }
+
+        public bool? Estado { get; set; }
+
+        public DateTime? FechaCreacion { get; set; }
+
+        public DateTime? FechaActualizacion { get; set; }
+
+        public string? CreadoPor { get; set; }
+
+        public string? ActualizadoPor { get; set; }
     }
+
 }
