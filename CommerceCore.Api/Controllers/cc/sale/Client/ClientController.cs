0b80cfe170f6c7dbe9ff936d6b5ffa6e1ff1f94f
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-02-14 06:45:36 -0600

    endpoint para leer clientes

---
+++ CommerceCore.Api/Controllers/cc/sale/Client/ClientController.cs
@@ -0,0 +1,32 @@
+﻿using CommerceCore.Api.Tools;
+using CommerceCore.BL.cc.logistics.warehouse;
+using Microsoft.AspNetCore.Mvc;
+using CommerceCore.BL.cc.sale.Clientes;
+
+namespace CommerceCore.Api.Controllers.cc.sale.Client
+{
+    [Route("clients")]
+    [ApiController]
+    public class ClientController : CustomController
+    {
+        private Clientes client = new Clientes(Tool.configuration);
+
+        /// <summary>
+        /// Devuelve las regiones existentes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("allClients")]
+        public IActionResult GetAllRegions()
+        {
+            try
+            {
+                var result = client.GetClient(userName);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ CommerceCore.BL/cc/sale/Clientes/Clientes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CC;
+using NuGet.Configuration;
+using CommerceCore.ML;
+using CommerceCore.DAL.Commerce;
+
+namespace CommerceCore.BL.cc.sale.Clientes
+{
+    public class Clientes : LogicBase
+    {
+        public Clientes(Configuration settings) {
+            configuration = settings;
+        }
+
+        public List<Cliente> GetClient(string userName)
+        {
+            try
+            {
+                using (SoftByte db = new SoftByte(configuration.appSettings.cadenaSql))
+                {
+                    var clients = new List<Cliente>();
+                    clients = db.Clientes.Where(c => c.Activo == true ).ToList();
+                    return clients;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        
+        }
+    }
+}
