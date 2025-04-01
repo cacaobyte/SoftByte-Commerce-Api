6b3b83926c3019fdb486694b974027ed95889562
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-04-01 06:54:10 -0600

    correcion de quotes y manejo de home

---
+++ CommerceCore.Api/Controllers/cc/Home/WelcomeController.cs
@@ -0,0 +1,52 @@
+﻿using CommerceCore.Api.Tools;
+using CommerceCore.BL.cc.sale.quotes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CommerceCore.Api.Controllers.cc.Home
+{
+    [Route("home")]
+    [ApiController]
+    public class WelcomeController : CustomController
+    {
+
+        /// <summary>
+        /// Devuelve los datos de metricas almacenados por el sistemas Saas
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("general")]
+        public IActionResult GetAllmetricsSaas()
+        {
+            try
+            {
+              //  var result = quotesBl.GetQuotesCacao(userName);
+                return Ok("");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Devuelve los datos de metricas almacenados por el sistemas Saas de mi compañias
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("company")]
+        public IActionResult GetAllMetricsCompany()
+        {
+            try
+            {
+                //  var result = quotesBl.GetQuotesCacao(userName);
+                return Ok("");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+    }
+}
