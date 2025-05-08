37d016f3388917910db21d73daf6164d56591603
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-05-07 22:28:12 -0600

    primera version del endpoint de menu

---
+++ CommerceCore.Api/Controllers/cc/Menu/MenuPlanController.cs
@@ -0,0 +1,33 @@
+﻿using CommerceCore.Api.Tools;
+using Microsoft.AspNetCore.Mvc;
+using CommerceCore.BL.cc.Menu;
+using CommerceCore.BL.cc.sale.quotes;
+
+namespace CommerceCore.Api.Controllers.cc.Menu
+{
+    [Route("menu")]
+    [ApiController]
+    public class MenuPlanController : CustomController
+    {
+
+        private MenuPlan menuBl = new MenuPlan(Tool.configuration);
+
+        /// <summary>
+        /// Devuelve el menu del plan
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("plan")]
+        public IActionResult GetAllQuotesCacaoByte()
+        {
+            try
+            {
+                var result = menuBl.GetMenuPlan(IdAplication, Plan);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ CommerceCore.BL/cc/Menu/MenuPlan.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CC;
+using NuGet.Configuration;
+using CommerceCore.ML;
+using CommerceCore.DAL.Commerce;
+using CommerceCore.BL.cc.cloudinary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using CommerceCore.ML.cc.sale.Quotes;
+
+namespace CommerceCore.BL.cc.Menu
+{
+    public class MenuPlan : LogicBase
+    {
+        public MenuPlan(Configuration settings) {
+            configuration = settings;
+        }
+
+        public dynamic GetMenuPlan(int aplicacion, string plan)
+        {
+            try
+            {
+                using (SoftByte db = new SoftByte(configuration.appSettings.cadenaSql))
+                {
+                    var menu = db.Menus
+                        .Join(db.Agrupadors, m => m.Id, a => a.Menu, (m, a) => new { m, a })
+                        .Join(db.Opcions, ma => ma.a.Id, o => o.Menu, (ma, o) => new
+                        {
+                            MenuId = ma.m.Id,
+                            MenuNombre = ma.m.Nombre,
+                            Aplicacion = ma.m.Aplicacion,
+                            Plan = ma.m.plan,
+                            AgrupadorId = ma.a.Id,
+                            AgrupadorNombre = ma.a.Nombre,
+                            OpcionId = o.Id,
+                            OpcionNombre = o.Nombre,
+                            OpcionRuta = o.Url
+                        })
+                        .Where(x => x.Aplicacion == aplicacion && x.Plan == plan)
+                        .ToList();
+
+                    return menu;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+
+    }
+}
